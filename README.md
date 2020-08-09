da55d8afee289017b866948518e220af2607bda8
Jay Wilder <jayswilder@yahoo.com>
2020-08-09 02:46:43 -0500

    initial upload

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # js211_Dodgeball
+We are in need of a sorting and organizing app for our community dodge ball league. There are already 6 players signed up and we hope to get more! We need to select from our currently signed-up people to make them dodge ball players and from there we need to be able to select them to be on different teams. Please look over the Specs Checklist to make sure you understand the needs of this app.
+
+20pts - Code Plan - Include this in a README.md file in your folder with comment in your code.
+20pts - Can add People to Players - When clicked the people are added to the Players column and removed from the People list while getting new values of a player added to them.
+20pts - Can add Players to different Teams - When we click on the blue button the Player is added to the blue team and removed from the Player list while also getting the keys color and mascot extended to them when they are moved to a team.
+20pts - Uses Class - This is not a hack job. You should use class to add the new properties you need and extend when you need.
+20pts - Minimum 3 Unit Tests - Use Mocha and Chai to give us at least 3 unit test that prove a person becomes a player and player becomes a teammate.
+
+
+Once you understand the challenge, whiteboard the logic.
+
+Create a list of steps your app needs to do. (Code Plan)
+
+Translate to psuedo code.
+
+Translate to javascript on paper
+
+Make a repo, clone, open and copy/paste the starter code below into two new files: index.html and main.js
+Start by working through the existing code to understand it and write comments explaining what each line does.
+
+Put your Code Plan in the README.md file or your repo
+Work through the follow challenges:
+    Use the class keyword to create a template of a dodgeBallPlayer that requires canThrowBall, canDodgeBall, hasPaid, isHealthy, yearsExperience
+    
+    Push these new dodge ball Player objects into a new array and then display them in the DOM as available players to pick.
+    
+    Add a button to each new player that will allow each one to be selected for either Blue Team or Read Team and now has mascot and teamColor
+    
+    Use the this keyword to assign each player to a team with an onclick. Either Blue Team or Red Team.
+    
+    Display the two teams in a new list in the DOM with appropriate titles.
+    
+    Create 3 tests for your application.
---
+++ main.js
@@ -0,0 +1,265 @@
+const arrOfPeople = [
+    {
+        id: 2,
+        name: "Charles Young",
+        age: 55,
+        skillSet: "welding",
+        placeBorn: "Omaha, Nebraska",
+        canThrowBall: true,
+        canDodgeBall: false,
+        hasPaid: true,
+        isHealthy: true,
+        yearsExperience: 3
+    },
+    {
+        id: 3,
+        name: "Judy Twilight",
+        age: 35,
+        skillSet: "fishing",
+        placeBorn: "Louisville, Kentucky",
+        canThrowBall: true,
+        canDodgeBall: false,
+        hasPaid: true,
+        isHealthy: true,
+        yearsExperience: 3
+    },
+    {
+        id: 4,
+        name: "Cynthia Doolittle",
+        age: 20,
+        skillSet: "tic tac toe",
+        placeBorn: "Pawnee, Texas",
+        canThrowBall: true,
+        canDodgeBall: false,
+        hasPaid: true,
+        isHealthy: true,
+        yearsExperience: 3
+    },
+    {
+        id: 5,
+        name: "John Willouby",
+        age: 28,
+        skillSet: "pipe fitting",
+        placeBorn: "New York, New York",
+        canThrowBall: true,
+        canDodgeBall: false,
+        hasPaid: true,
+        isHealthy: true,
+        yearsExperience: 3
+    },
+    {
+        id: 6,
+        name: "Stan Honest",
+        age: 20,
+        skillSet: "boom-a-rang throwing",
+        placeBorn: "Perth, Australia",
+        canThrowBall: true,
+        canDodgeBall: false,
+        hasPaid: true,
+        isHealthy: true,
+        yearsExperience: 3
+    },
+    {
+        id: 7,
+        name: "Mia Watu",
+        age: 17,
+        skillSet: "acrobatics",
+        placeBorn: "Los Angeles, California",
+        canThrowBall: true,
+        canDodgeBall: false,
+        hasPaid: true,
+        isHealthy: true,
+        yearsExperience: 3
+    },
+    {
+        id: 8,
+        name: "Walter Cole",
+        age: 32,
+        skillSet: "jump rope",
+        placeBorn: "New Orleans, Louisiana",
+        canThrowBall: true,
+        canDodgeBall: false,
+        hasPaid: true,
+        isHealthy: true,
+        yearsExperience: 3
+    },
+]
+
+let blue = document.getElementById('blue');
+let red = document.getElementById('red');
+
+const listOfPlayers = []
+const blueTeam = []
+const redTeam = []
+
+// template for creating new players
+
+class Player {
+    constructor(id, name, age, skillSet, placeBorn, canThrowBall, canDodgeBall, hasPaid, isHealthy, yearsExperience) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.skillSet = skillSet;
+        this.placeBorn = placeBorn;
+        this.canDodgeBall = canThrowBall;
+        this.canDodgeBall = canDodgeBall;
+        this.hasPaid = hasPaid;
+        this.isHealthy = isHealthy;
+        this.yearsExperience = yearsExperience;
+    }
+}
+
+// template for putting a person onto the blue team and giving them a mascot and team color
+
+class blueTeammate extends Player {
+    constructor(id, name, age, skillSet, placeBorn, canThrowBall, canDodgeBall, hasPaid, isHealthy, yearsExperience, mascot, teamColor) {
+        super(id, name, age, skillSet, placeBorn, canThrowBall, canDodgeBall, hasPaid, isHealthy, yearsExperience);
+        this.mascot = mascot;
+        this.teamColor = teamColor;
+    }
+}
+
+// template for putting a person onto the red team and giving them a mascot and team color
+
+class redTeammate extends Player {
+    constructor(id, name, age, skillSet, placeBorn, canThrowBall, canDodgeBall, hasPaid, isHealthy, yearsExperience, mascot, teamColor) {
+        super(id, name, age, skillSet, placeBorn, canThrowBall, canDodgeBall, hasPaid, isHealthy, yearsExperience);
+        this.mascot = mascot;
+        this.teamColor = teamColor;
+    }
+}
+
+// function to move person from list of players to the blue team
+
+const createBlueTeammate = (id) => {
+    const listElement = document.getElementById('blue')
+    const li = document.createElement("li")
+    console.log(id)
+
+    listOfPlayers.find(person => {
+        // check to see if the id (list element) that is clicked is the same as the person's id
+        if (id == person.id) {
+            // create a new blue teammate and assign their attributes
+            const newBlueTeammate = new blueTeammate(person.id, person.name, person.age, person.skillSet, person.placeBorn, person.canThrowBall, person.canDodgeBall, person.hasPaid, person.isHealthy, person.yearsExperience, "Leviathan", 'blue')
+            // push the new blue teammate to the blueTeam array
+            blueTeam.push(newBlueTeammate)
+            console.log(blueTeam)
+            // create a new li element to the dom for the blue team
+            const li = document.createElement("li")
+
+            li.appendChild(document.createTextNode(person.name + " - " + person.skillSet))
+            listElement.append(li)
+        }
+    })
+
+    console.log("Person added to blue team")
+}
+
+// function to move person from list of players to the red team 
+const createRedTeammate = (id) => {
+
+    const listElement = document.getElementById('red')
+    const li = document.createElement("li")
+    console.log(id)
+
+    listOfPlayers.find(person => {
+        // check to see if the 'id'(list element) that is clicked is the same as the person's id
+        if (id == person.id) {
+            // push the new red teammate to the redTeam array
+            const newRedTeammate = new redTeammate(person.id, person.name, person.age, person.skillSet, person.placeBorn, person.canThrowBall, person.canDodgeBall, person.hasPaid, person.isHealthy, person.yearsExperience, "Red Devil", 'red')
+            // push the new red teammate to the redTeam array            
+            redTeam.push(newRedTeammate)
+            console.log(redTeam)
+            // create a new li element to the dom for the red team            
+            const li = document.createElement("li")
+
+            li.appendChild(document.createTextNode(person.name + " - " + person.skillSet))
+            listElement.append(li)
+        }
+    })
+
+    console.log("Person added to red team")
+}
+
+
+// function to populate people on the dom from the arrOfPeople
+const listPeopleChoices = () => {
+    const listElement = document.getElementById('people')
+    listElement.innerHTML = ''
+    arrOfPeople.map(person => {
+        const li = document.createElement("li")
+        const button = document.createElement("button")
+        button.innerHTML = "Make Player"
+        button.addEventListener('click', function () {
+            makePlayer(person.id)
+            listElement.removeChild(li)
+        })
+        li.appendChild(button)
+        li.appendChild(document.createTextNode(person.name + " - " + person.skillSet))
+        listElement.append(li)
+    })
+}
+
+const makePlayer = (id) => {
+
+    const listElement = document.getElementById('players')
+    console.log(arrOfPeople)
+    arrOfPeople.find(person => {
+
+        if (id == person.id) {
+            listOfPlayers.push(person)
+            console.log(listOfPlayers)
+            const li = document.createElement("li")
+
+            li.appendChild(document.createTextNode(person.name + " - " + person.skillSet))
+            listElement.append(li)
+
+            const buttonBlue = document.createElement("button")
+            buttonBlue.addEventListener('click', function () {
+                createBlueTeammate(person.id)
+                listElement.removeChild(li)
+            })
+
+            buttonBlue.innerHTML = "Blue Team"
+            li.appendChild(buttonBlue)
+
+            const buttonRed = document.createElement("button")
+
+            buttonRed.addEventListener('click', function () {
+                createRedTeammate(person.id)
+                listElement.removeChild(li)
+            })
+
+            buttonRed.innerHTML = "Red Team"
+            li.appendChild(buttonRed)
+
+        }
+    })
+    console.log(`li ${id} was clicked!`)
+}
+// id, name, age, skillSet, placeBorn, canThrowBall, canDodgeBall, hasPaid, isHealthy, yearsExperience
+const createNewPerson = () => {
+    const personId = arrOfPeople.length + 2
+    const personName = document.getElementById('personName').value
+    const personAge = document.getElementById('personAge').value
+    const personSkillSet = document.getElementById('skillSet').value
+    const personPlaceBorn = document.getElementById('placeBorn').value
+    const personThrow = document.getElementById('throw').value
+    const personDodge = document.getElementById('dodge').value
+    const personPayment = document.getElementById('payment').value
+    const personHealthy = document.getElementById('healthy').value
+    const personExperience = document.getElementById('experience').value
+
+    if (personId == '' || personName == '' || personAge == '' || personSkillSet == '' || personPlaceBorn == '' || personExperience == '') {
+        alert('Please enter your information for registry.')
+    } else {
+        const newPerson = new Player(personId, personName, personAge, personSkillSet, personPlaceBorn, personThrow, personDodge, personPayment, personHealthy, personExperience)
+        arrOfPeople.push(newPerson)
+        console.log(arrOfPeople)
+        document.getElementById('personName').value = ''
+        document.getElementById('personAge').value = ''
+        document.getElementById('skillSet').value = ''
+        document.getElementById('placeBorn').value = ''
+        document.getElementById('experience').value = ''
+    }
+}
